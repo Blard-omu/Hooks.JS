fd2ca27ea70b40a2139228947f95dc260d331844
Blard <peteromu76@gmail.com>
2022-05-20 13:31:50 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Display from './Display';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+      
+      <Display/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Display.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import cc from './favicon-32x32.png'
+import dd from './profile_3.jpg'
+
+
+
+
+const Display = () => {
+
+    let [data, setData] = useState( [
+        { id: 1, title: 'Blessing Tech', body: 'we build web apps', author: 'Blessing', img: cc},
+           
+        {id: 2, title: 'Silvanus Tech', body: 'we product designs', author: 'Silva', img: dd},
+    
+        {id: 3, title: 'Rilwans consult', body: 'we carry out therapeutic consultations', author: 'Rilwan', img: cc},
+    
+        {id: 4, title: 'B-lard computers', body: 'we procure hardwares and software apps', author: 'B-lard', img: dd}
+    ])
+function removeItem(id){
+    let remove = data.filter((detail)=> detail.id !== id)
+    setData(remove)}
+
+    function commotAll(){
+        setData([])
+    }
+
+  return (
+    <div>
+        {data.map((datum)=>{
+            const {title,body,author,id,img} = datum
+            return(
+                <div className="move" key={id}>
+                    <h2>{title}</h2>
+                    <h2>{body}</h2>
+                    <h2>{author}</h2>
+                    <button className='clickBtn' onClick={()=> removeItem(datum.id)}>click me</button>
+                    <div className="">
+                        <img src={img} alt="" srcset="" />
+                    </div>
+
+                </div>
+            )
+        })
+
+        }
+<button className='clearBtn' onClick={commotAll} >Clear</button>
+
+
+
+        <div className="maq">
+        <marquee behavior="scroll" direction="left">
+            <h3>Welcome to blard Page</h3>
+        </marquee>
+        </div>
+
+    </div>
+  )
+}
+
+export default Display
